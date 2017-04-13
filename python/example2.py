b4e6b097428b59ba01bf3e605396c628e1f29ae1
matthew-groth <mgroth49@gmail.com>
2017-04-13 14:54:25 -0400

    changed example2 output to correct format, but still occasionally prints bad output. gotta remove that.

--- python/example2.py
+++ python/example2.py
@@ -11,5 +11,18 @@ if __name__ == "__main__":
         while True:
             packet = headset.dequeue()
             if packet is not None:
-                pass
+                print str(packet.sensors['F3']['value']) + " " + \
+                  str(packet.sensors['F4']['value']) + " " + \
+                  str(packet.sensors['P7']['value']) + " " + \
+                  str(packet.sensors['FC6']['value']) + " " + \
+                  str(packet.sensors['F7']['value']) + " " + \
+                  str(packet.sensors['F8']['value']) + " " + \
+                  str(packet.sensors['T7']['value']) + " " + \
+                  str(packet.sensors['P8']['value']) + " " + \
+                  str(packet.sensors['FC5']['value']) + " " + \
+                  str(packet.sensors['AF4']['value']) + " " + \
+                  str(packet.sensors['T8']['value']) + " " + \
+                  str(packet.sensors['O2']['value']) + " " + \
+                  str(packet.sensors['O1']['value']) + " " + \
+                  str(packet.sensors['AF3']['value'])
             time.sleep(0.001)
